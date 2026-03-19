e9dec17e9bd0256689777ee8e94ff8e2411b9412
Vasili Vushau <vushauv@gmail.com>
2026-03-19 16:34:47 +0100

    hw1: feat: task3 solved

--- homeworks/hw1/Solution.hs
+++ homeworks/hw1/Solution.hs
@@ -16,15 +16,36 @@ coprimePairs (x:xs) = [(min x y, max x y) | y <- xs, gcd x y == 1, x /= y] ++ co
 -- (ordered selections without repetition) from a given list.
 -- For example, for k = 2 and list [1,2,3] the result should be [[1,2],[1,3],[2,1],[2,3],[3,1],[3,2]]
 
--- permutations :: Int -> [a] -> [[a]]
--- permutations 0 _      = []
--- permutations n []     = []
--- permutations 1 (x:xs) = [[x]] ++ permutations 1 xs 
--- permutations n (x:xs) = [[
-
 removeAt :: Int -> [a] -> [a]
 removeAt i xs = take i xs ++ drop (i+1) xs
 
 permutations :: Int -> [a] -> [[a]]
 permutations 0 _  = [[]]
 permutations k xs = [x:rest | (i, x) <- zip [0..] xs, rest <- permutations (k-1) (removeAt i xs)]
+
+
+-- Task 3
+-- Sieve of Eratosthenes The Sieve of Eratosthenes is an ancient algorithm for finding all primes up to a given limit.
+-- It works as follows: starting from the list [2..n], take the first element p — it must be prime — then remove all multiples of p from the rest of the list and repeat.
+
+-- Implement this as a recursive function sieve :: [Int] -> [Int], where each recursive step uses a list comprehension to filter out multiples of the head. Then define:
+-- primesTo :: Int -> [Int]
+-- primesTo n = sieve [2..n]
+
+-- Finally, use primesTo to define isPrime :: Int -> Bool that checks whether a given positive integer is prime.
+
+sieve :: [Int] -> [Int]
+sieve []   = []
+sieve (x:xs) = [x] ++ sieve [xi | xi <- xs, xi `mod` x /= 0]
+
+primesTo :: Int -> [Int]
+primesTo n = sieve [2..n]
+
+isPrime :: Int -> Bool
+isPrime x
+   | x < 2 = False
+   | otherwise = last (primesTo x) == x
+
+
+
+
