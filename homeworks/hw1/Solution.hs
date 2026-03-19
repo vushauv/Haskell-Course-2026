a9f4b1f978e54ec7ef56fd9c7313e680812818a9
Vasili Vushau <vushauv@gmail.com>
2026-03-19 16:05:01 +0100

    hw1: feat: task5 solved

--- homeworks/hw1/Solution.hs
+++ homeworks/hw1/Solution.hs
@@ -7,3 +7,24 @@
 coprimePairs :: [Int] -> [(Int, Int)]
 coprimePairs [] = []
 coprimePairs (x:xs) = [(min x y, max x y) | y <- xs, gcd x y == 1, x /= y] ++ coprimePairs xs
+
+
+
+
+-- Task 5
+-- Permutations. Write a function permutations :: Int -> [a] -> [[a]] that generates all k-element permutations
+-- (ordered selections without repetition) from a given list.
+-- For example, for k = 2 and list [1,2,3] the result should be [[1,2],[1,3],[2,1],[2,3],[3,1],[3,2]]
+
+-- permutations :: Int -> [a] -> [[a]]
+-- permutations 0 _      = []
+-- permutations n []     = []
+-- permutations 1 (x:xs) = [[x]] ++ permutations 1 xs 
+-- permutations n (x:xs) = [[
+
+removeAt :: Int -> [a] -> [a]
+removeAt i xs = take i xs ++ drop (i+1) xs
+
+permutations :: Int -> [a] -> [[a]]
+permutations 0 _  = [[]]
+permutations k xs = [x:rest | (i, x) <- zip [0..] xs, rest <- permutations (k-1) (removeAt i xs)]
