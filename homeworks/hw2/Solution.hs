93e43727127549b7c1a12475827d399bcd643b07
Vasili Vushau <vushauv@gmail.com>
2026-04-05 19:12:10 +0200

    feat(hw2): task 1 (functor)

--- homeworks/hw2/Solution.hs
+++ homeworks/hw2/Solution.hs
@@ -1,7 +1,12 @@
 data Sequence a = Empty | Single a | Append (Sequence a) (Sequence a)
+   deriving Show
 
 
+
+-- Task 1
 -- Write a Functor instance for Sequence:
 
 instance Functor Sequence where
-    fmap :: (a -> b) -> Sequence a -> Sequence b
+   fmap f Empty        = Empty
+   fmap f (Single a)   = Single(f a)
+   fmap f (Append h t) = Append (fmap f h) (fmap f t)
