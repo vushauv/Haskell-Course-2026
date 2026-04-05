e499b3cff816fb186bb28f637db68573470f9956
Vasili Vushau <vushauv@gmail.com>
2026-04-05 19:36:46 +0200

    feat(hw2): task 2 (foldable)

--- homeworks/hw2/Solution.hs
+++ homeworks/hw2/Solution.hs
@@ -1,3 +1,5 @@
+import Data.Sequence (Seq)
+import Data.Foldable (Foldable(toList))
 data Sequence a = Empty | Single a | Append (Sequence a) (Sequence a)
    deriving Show
 
@@ -10,3 +12,22 @@ instance Functor Sequence where
    fmap f Empty        = Empty
    fmap f (Single a)   = Single(f a)
    fmap f (Append h t) = Append (fmap f h) (fmap f t)
+
+
+-- Task 2
+-- Write a Foldable instance for Sequence by implementing foldMap:
+
+
+-- foldMap :: Monoid m => (a -> m) -> Sequence a -> m
+instance Foldable Sequence where
+   foldMap f Empty        =  mempty 
+   foldMap f (Single a)   = f a
+   foldMap f (Append l r) = (foldMap f l) <> (foldMap f r)
+
+seqToList :: Sequence a -> [a]
+seqToList a = toList a
+
+
+seqLength :: Sequence a -> Int
+seqLength a = length a 
+
