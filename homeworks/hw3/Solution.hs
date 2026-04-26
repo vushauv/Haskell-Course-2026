30b0e67bdd00fa964afac0081ac74c6c3d5d531d
Vasili Vushau <vushauv@gmail.com>
2026-04-26 21:12:20 +0200

    hw3: task 1.b and 1.c

--- homeworks/hw3/Solution.hs
+++ homeworks/hw3/Solution.hs
@@ -1,4 +1,5 @@
 import qualified Data.Map as Map
+import Control.Monad (foldM)
 
 -- Task 1 Maybe Monad
 
@@ -9,3 +10,15 @@ type Maze = Map.Map Pos (Map.Map Dir Pos)
 -- (a)
 move :: Maze -> Pos -> Dir -> Maybe Pos
 move maze pos dir = Map.lookup pos maze >>= Map.lookup dir
+
+-- (b)
+followPath :: Maze -> Pos -> [Dir] -> Maybe Pos
+followPath maze start dirs = foldM (move maze) start dirs
+
+-- (c)
+safePath :: Maze -> Pos -> [Dir] -> Maybe [Pos]
+safePath maze start dirs = fmap reverse $ foldM step [start] dirs
+  where
+    step acc@(pos : _) dir = do
+      next <- move maze pos dir
+      return (next : acc)
