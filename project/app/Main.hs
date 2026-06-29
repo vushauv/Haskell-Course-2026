c3f10821ad946167c434dc99cd820e6ab32c7b79
Vasili Vushau <vushauv@gmail.com>
2026-06-29 05:56:51 +0200

    feat: add main

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -1,3 +1,14 @@
 module Main (main) where
+
+import Mdethodology.Site.Pipeline (runPipeline, defaultPipeline)
+import Mdethodology.Types (Site(..), YamlValue(..))
+import System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn "mdethodology"
+main = do
+  args <- getArgs
+  case args of
+    ("build" : src : "-o" : out : _) -> do
+      _ <- runPipeline (defaultPipeline src out) (Site [] YNull)
+      putStrLn ("built " ++ src ++ " -> " ++ out)
+    _ -> putStrLn "usage: mdethodology build <src> -o <out>"
