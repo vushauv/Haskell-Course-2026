493b0eec13ec46f5ff8d29ff3bd4ce3e8dfc861e
Vasili Vushau <vushauv@gmail.com>
2026-06-29 03:27:45 +0200

    feat: add Page, Site and Pass types

--- project/src/Mdethodology/Types.hs
+++ project/src/Mdethodology/Types.hs
@@ -11,12 +11,14 @@ data YamlValue
    | YMap    (Map String YamlValue)
    deriving (Eq, Show)
 
+
 data Block
   = Heading Int [Inline]            -- '#' level + the heading text
   | Paragraph [Inline]
   | CodeBlock (Maybe String) String -- optional language tag + raw code
   | BulletList [[Block]]            -- each item is itself a list of blocks
   deriving (Eq, Show)
+
 
 data Inline
   = Text   String
@@ -25,6 +27,7 @@ data Inline
   | Code   String                   -- `inline code`
   | Link   [Inline] LinkTarget      -- link text + where it points
   deriving (Eq, Show)
+
 
 data LinkTarget
   = External String                 -- http://...  — leave alone
@@ -38,5 +41,22 @@ data SourceFile = SourceFile
   , srcBody        :: [Block]
   } deriving (Eq, Show)
 
+
 newtype Route = Route { unRoute :: String }
   deriving (Eq, Ord, Show)
+
+
+data Page = Page
+  { pageSource   :: SourceFile
+  , pageRoute    :: Route
+  , pageTemplate :: String
+  } deriving (Eq, Show)
+
+
+data Site = Site
+  { sitePages  :: [(Route, Page)]
+  , siteConfig :: YamlValue
+  } deriving (Eq, Show)
+
+-- A build step: take the site, do some IO, return the updated site.
+type Pass = Site -> IO Site
