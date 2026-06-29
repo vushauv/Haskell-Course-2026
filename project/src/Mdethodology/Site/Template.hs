0e3235fcc2682e660ef8148b466cbb6b6676f888
Vasili Vushau <vushauv@gmail.com>
2026-06-29 14:55:35 +0200

    feat: allow literal braces in templates

--- project/src/Mdethodology/Site/Template.hs
+++ project/src/Mdethodology/Site/Template.hs
@@ -7,13 +7,16 @@ import qualified Data.Map as Map
 data Chunk = Lit String | Hole String
 
 template :: Parser [Chunk]
-template = many (hole <|> lit)
+template = many (hole <|> lit <|> loneBrace)
   where
     hole = do _ <- string "{{"; spaces
               name <- some (satisfy (`notElem` " }"))
               spaces; _ <- string "}}"
               pure (Hole name)
     lit  = Lit <$> some (satisfy (/= '{'))
+    -- a single '{' (not starting a hole, since `hole` is tried first) is literal,
+    -- so template bodies may contain CSS/JS braces
+    loneBrace = Lit . (: []) <$> char '{'
 
 renderTemplate :: Map.Map String String -> String -> Either ParseError String
 renderTemplate scope src = do
